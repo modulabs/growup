133a6813e00871fb9eca8c0ffac427ecf0399c12
Admin <admin@Adminui-MacBookAir.local>
2026-02-09 10:59:26 +0900

    feat: Server-side frontend hosting config (backup for SSL issues)

---
+++ deploy_gh_pages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd frontend/build
+rm -rf .git
+git init
+git add .
+git commit -m "Deploy to gh-pages"
+git remote add origin git@github.com:modulabs/growup.git
+git push -f origin master:gh-pages
