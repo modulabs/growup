fd97b14d757fa5d276ff5b6daa7ab2bd1688d369
ParkHyoungChul <asdhk0762@gmail.com>
2026-02-24 16:05:05 +0900

    fix: 배포 프론트 API 기본 URL을 ddns growup 경로로 반영

---
+++ _app/immutable/chunks/BECQi3MN.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_egq6ur)==null?void 0:s.base)??"/growup";var e;const t=((e=globalThis.__sveltekit_egq6ur)==null?void 0:e.assets)??a??"";export{t as a,a as b};
